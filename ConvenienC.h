8b40a1b17bc6723d812806e29a845143908a6aa7
shipof123 <34876781+shipof123@users.noreply.github.com>
2019-02-04 15:30:31 -0500

    Update ConvenienC.h

--- ConvenienC.h
+++ ConvenienC.h
@@ -1,7 +1,29 @@
 #ifndef CONV_C_H_
 #define CONV_C_H_
 
-typedef void* anything;
+#define _attr(a) __attribute ((a))
+
+#define STR(s) #s
+#define STRLEN(s) 
+
+
+#if __GNUC__ >= 3
+  #undef  inline 
+  #define inline        inline _attr(always_inline)
+  #define __noinline    _attr(noinline)
+  #define __pure        _attr(pure)
+  #define __const       _attr(const)
+  #define __noreturn    _attr(noreturn)
+  #define __malloc      _attr(malloc)
+  #define __must_check  _attr(warn_unused_result)
+  #define __deprecated  _attr(deprecated)
+  #define __used        _attr(used)
+  /* TODO */
+#endif
+
+#ifdef  __cplusplus
+  #define var auto
+#endif
 
 #define unless(expr) if(!(expr))
 #define until(expr)  while(!(expr))
